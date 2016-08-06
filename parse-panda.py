d9d1f79f28be69028e89857e65328e63f0ebddd3
Dan Guest <dguest@cern.ch>
2016-08-06 02:26:43 +0200

    This damn thing still doesn't work
    I can't tell if I'm doing something wrong or the panda rest api is
    just a pile of crap...

--- parse-panda.py
+++ parse-panda.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
-from urllib import request
+from urllib import request, parse
 import json
+import sys
+import re
+
+_ok_states = {'done', 'finished'}
 
 def run():
-    url = 'http://bigpanda.cern.ch/tasks/?taskname=user.dguest*&days=8'
-    headers = {'Accept:': 'application/json',
-               'Content-Type:':'application/json'}
+    taskname_tag = 'v5.holistic'
+    params = parse.urlencode({
+        # 'taskname': 'user.dguest*',
+        # 'taskname': 'group.perf-flavtag*',
+        'taskid': 9147919,
+        # 'days': 5,
+        # 'produsername': 'Daniel Hay Guest',
+        # 'datasets': 'yes'
+    })
+    print(params)
+    # return 0
+    url = 'http://bigpanda.cern.ch/tasks/?' + params
+    headers = {'Accept': 'application/json',
+               'Content-Type':'application/json',
+               'User-Agent':'User-Agent: curl/7.43.0'}
     req = request.Request(url, headers=headers)
     stuff = request.urlopen(req).read().decode('utf-8')
-    dic = json.loads(stuff)
-    print(json.dumps(dic, indent=2))
+    print(json.dumps(json.loads(stuff), indent=2))
+    for entry in json.loads(stuff):
+        # if entry['status'] in _ok_states:
+        #     continue
+        # if taskname_tag not in entry['taskname']:
+        #     continue
+        print(entry)
+        for ds in entry['datasets']:
+            print(ds['datasetname'])
+
+
+
 
 if __name__ == '__main__':
     run()
