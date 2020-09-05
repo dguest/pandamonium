bed92d13ed63a7fd0dc42a21829b1be7fa619aee
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-05 01:21:28 -0500

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+extras_require = {}
+extras_require["lint"] = sorted(set(["pyflakes", "black"]))
+extras_require["test"] = sorted(
+    set(
+        [
+            "check-manifest",
+            "pytest~=6.0",
+            "pytest-cov~=2.8",
+            "pytest-console-scripts~=0.2",
+            "pytest-mock~=3.0",
+        ]
+    )
+)
+extras_require["develop"] = sorted(
+    set(
+        extras_require["test"]
+        + extras_require["lint"]
+        + ["check-manifest", "bumpversion~=0.5", "pre-commit", "twine"]
+    )
+)
+extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
+
+setup(
+    extras_require=extras_require,
+    use_scm_version=lambda: {"local_scheme": lambda version: ""},
+)
