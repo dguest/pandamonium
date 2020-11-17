53e385daabfd528ed8dac07b7e5e67432d4d8eb6
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-17 03:09:13 -0600

    Add start of tests

---
+++ tests/test_pandamon.py
@@ -0,0 +1,10 @@
+import os
+from pandamonium import pandamon
+
+
+def test_get_args():
+    os.environ["USER"] = "pytest"
+    args = pandamon.get_args()
+    assert args.user == "pytest"
+    assert args.taskname == "user.pytest"
+    assert args.username == ""
