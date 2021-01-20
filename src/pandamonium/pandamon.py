11d27b53978933c83dee9dcbae52ea1ed0349eac
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-20 13:40:06 -0600

    Turn on float-to-top to move imports to top

--- src/pandamonium/pandamon.py
+++ src/pandamonium/pandamon.py
@@ -11,22 +11,23 @@ The user name can be specified via environment variable to reduce
 clutter.
 """
 
-# Attempt to default to Python 3
-try:
-    from urllib.parse import urlencode
-    from urllib.request import Request
-    from urllib.request import urlopen
-except:
-    from urllib2 import urlopen
-    from urllib2 import Request
-    from urllib import urlencode
-
 import argparse
 import datetime
 import json
 import os
 import re
 import sys
+
+# Attempt to default to Python 3
+try:
+    from urllib.parse import urlencode
+    from urllib.request import Request
+    from urllib.request import urlopen
+except:
+    from urllib import urlencode
+
+    from urllib2 import Request
+    from urllib2 import urlopen
 
 # help strings
 _h_taskname = 'initial search string'
